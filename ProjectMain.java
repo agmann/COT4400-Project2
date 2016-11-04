786f7abf6a957f0dbb106aab63d71661ceb33ffa
Austin Mann <amann@prnhelpdesk.com>
2016-11-04 16:04:23 -0400

    init

---
+++ ProjectMain.java
@@ -0,0 +1,10 @@
+/* This is the java source file used for our project 2 submission. 
+ *
+ */
+ public class ProjectMain {
+
+     public static void main(String args[]) {
+         System.out.println("Men's Asses");
+     }
+
+ }
