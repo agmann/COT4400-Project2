57bb309d904edbac8553393878629b6862e27c93
Austin Mann <amann@prnhelpdesk.com>
2016-11-08 18:12:14 -0500

    improved logic

--- ProjectMain.java
+++ ProjectMain.java
@@ -7,11 +7,9 @@ import java.util.Arrays;
  public class ProjectMain {
 
      public static void main(String args[]) {
-         System.out.println("Hello World!");
+        System.out.println("Hello World!");
 
-        int[][] totals = new int[5][5]; 
-
-        int[] b = {5,6,4};
+        int[] b = {6,5,4};
 
         int test = populate(b,10);
 
@@ -19,27 +17,49 @@ import java.util.Arrays;
 
 
      }
-
+     /* FIXME: uggghhh
+      *
+      */
      public static int populate(int[] a, int t) {
          if (a.length == 1) {
-            return (t - a[0]); 
+             System.out.println("Size is 1!");
+             int ineq = (t - a[0])*(t - a[0]);
+             System.out.println("ineq is " + ineq);
+             return ineq;
          }
-         int lhs = (t - a[0]) * (t - a[0]);
-         int rhs = populate(Arrays.copyOfRange(a,1,a.length-1),t);
-         int ineq = lhs + rhs; 
+         System.out.println("Size isn't' 1!");
+         System.out.println("This: " + a[0] + " and " + a[1]);
+         int ineq = (t - a[0])*(t - a[0]) + populate(Arrays.copyOfRange(a,1,a.length),t); 
          int min = ineq;
-         int i = 0;
-         while (i < a.length-1) {
-             if (a[i] + a[i+1] <= t) {
-                lhs = (t - (a[i]+a[i+1])) * (t - (a[i]+a[i+1]));
-                rhs = populate(Arrays.copyOfRange(a,1,a.length-1),t);
-                ineq = lhs + rhs; 
-                if (ineq < min) 
-                    min = ineq;
-                i++;
-             }
-         } 
+         for (int i = 0; i < a.length-1; i++) {
+             int maxlength = findMaxSub(a, t, i+2);
+             if (maxlength <= 1)
+                return min;
+             int sum = findSubSum(a, maxlength);
+             ineq = (t - sum)* (t - sum) + populate(Arrays.copyOfRange(a,maxlength-1,a.length),t);
+             if (ineq < min)
+                min = ineq;
+         }
          return min;
+     }
+
+     private static int findMaxSub(int[] a, int t, int bound) {
+         int sum = 0;
+         for (int i = 0; i < bound; i++) {
+             sum += a[i];
+             if (sum > t) {
+                 return i;
+             }
+         }
+         return bound;
+     }
+
+     private static int findSubSum(int[] a, int maxlen) {
+         int sum = 0;
+         for (int x : a) {
+             sum += x;
+         }
+         return sum;
      }
 
  }
