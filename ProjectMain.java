2dbf6938607c07d2acb1089eccd8ed1d17ed8498
Austin Mann <amann@prnhelpdesk.com>
2016-11-11 11:04:36 -0500

    improved logic, correct results are returned

--- ProjectMain.java
+++ ProjectMain.java
@@ -10,6 +10,8 @@ import java.util.Arrays;
         System.out.println("Hello World!");
 
         int[] b = {6,5,4};
+        int[] c = {7};
+        int[] d = {7,3};
 
         int test = populate(b,10);
 
@@ -22,44 +24,52 @@ import java.util.Arrays;
       */
      public static int populate(int[] a, int t) {
          if (a.length == 1) {
-             System.out.println("Size is 1!");
+             //System.out.println("length is 1");
              int ineq = (t - a[0])*(t - a[0]);
-             System.out.println("ineq is " + ineq);
+             //System.out.println("returned " + ineq);
              return ineq;
          }
-         System.out.println("Size isn't' 1!");
-         System.out.println("This: " + a[0] + " and " + a[1]);
-         int ineq = (t - a[0])*(t - a[0]) + populate(Arrays.copyOfRange(a,1,a.length),t); 
+         int ineq = (t - a[0])*(t - a[0]) + populate(Arrays.copyOfRange(a,1,a.length),t);
          int min = ineq;
-         for (int i = 0; i < a.length-1; i++) {
-             int maxlength = findMaxSub(a, t, i+2);
-             if (maxlength <= 1)
-                return min;
-             int sum = findSubSum(a, maxlength);
-             ineq = (t - sum)* (t - sum) + populate(Arrays.copyOfRange(a,maxlength-1,a.length),t);
-             if (ineq < min)
-                min = ineq;
+         //Known that a.length is at least 2, so attempt to consolidate
+         for (int i = 2; i <= a.length; i++) {
+             if (t >= getSubsetSum(a,i)) {
+                 //System.out.println("Up to " + i + " items can be combined");
+                 int sum = getSubsetSum(a,i);
+                 //System.out.println("sum is " + sum);
+                 int ineqb = (t - sum)*(t - sum);
+                 //System.out.println("ineq is " + ineqb);
+                 if (i <= a.length-1) {
+                     //System.out.println("recurse...");
+                     ineqb += populate(Arrays.copyOfRange(a,i,a.length),t);
+                 }
+                 if (ineqb < min) {
+                     //System.out.println("ineq (" + ineqb + ") is less than min (" + min + ")");
+                     min = ineqb;
+                 }
+             }
          }
+         //System.out.println("returning " + min);
          return min;
      }
 
-     private static int findMaxSub(int[] a, int t, int bound) {
-         int sum = 0;
-         for (int i = 0; i < bound; i++) {
-             sum += a[i];
-             if (sum > t) {
-                 return i;
-             }
-         }
-         return bound;
+     private static int getScore(int[] a, int t, int sizeSubset) {
+         int sum = getSubsetSum(a, sizeSubset);
+         return (t - sum)*(t - sum);
      }
 
-     private static int findSubSum(int[] a, int maxlen) {
+     private static int getSubsetSum(int[] a, int sizeSubset) {
          int sum = 0;
-         for (int x : a) {
-             sum += x;
+         if (a.length < sizeSubset) {
+             return sum; 
+         }
+         for (int i = 0; i < sizeSubset; i++) {
+             sum += a[i];
          }
          return sum;
+         
      }
+
+
 
  }
