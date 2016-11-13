9c0b95367cd2b29dcf4c91e8ce825b5ee7b6aecd
jonathanValencia <jonathan.valencia716@gmail.com>
2016-11-13 16:06:50 -0500

    added reading from the file and an output file. added the input.txt file from the assignment.

--- ProjectMain.java
+++ ProjectMain.java
@@ -1,59 +1,62 @@
 /* This is the java source file used for our project 2 submission. 
  *
  */
+import java.io.File;
+import java.io.IOException;
 import java.util.Collections.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ProjectMain implements Helpers {
 
      public static void main(String args[]) {
 
+         //test data
         int[] b = {6,5,4};
         int[] c = {7};
         int[] d = {7,3};
         int[] e = {5,2,3,4,10,5,3};
         int[] f = {8,2,3,4,5,9,1,10,3,4,2,8};
-    
-        int test1;
-        test1 = IterApproach.FindOptimalScore(e,10);
-     
-        int test2 = FindOptimalScore(b,10);
-
-        System.out.println("Recursive test out: " + test2);
-        System.out.println("Iterative test out: " + test1);
-
-
 
          Scanner scan = null;
+         File inputFile = null;
+         File outputFile = null;
 
          try {
-             File file = new File("input.txt");
-             scan = new Scanner(file);
+             inputFile = new File("input.txt");
+             outputFile = new File("output.txt");
+             scan = new Scanner(inputFile);
          }catch (Exception ex){
-             System.out.println("file not found");
+             System.out.println("Error with the input/output files.");
          }
 
-        int numOfProblems = Integer.parseInt(scan.nextLine().trim());
-        int arrLength = scan.nextInt();
-        int t = Integer.parseInt(scan.nextLine().trim());
-         String[] arrayString = scan.nextLine().trim().split(" ");
-         int[] arr = new int[arrLength];
-         for (int i = 0; i < arr.length; i++) {
-             arr[i] = Integer.parseInt(arrayString[i]);
+         int numOfProblems = Integer.parseInt(scan.nextLine().trim());
+
+         for(int problems = 0; problems < numOfProblems; problems++) {
+
+             int arrLength = scan.nextInt();
+             int t = Integer.parseInt(scan.nextLine().trim());
+
+             String[] arrayString = scan.nextLine().trim().split(" ");
+             int[] arr = new int[arrLength];
+
+             for (int i = 0; i < arr.length; i++) {
+                 arr[i] = Integer.parseInt(arrayString[i]);
+             }
+
+             //from here we should be able to use arr as the file input
+             //once we are done testing.
+             int test1 = IterApproach.FindOptimalScore(e, 10);
+             int test2 = FindOptimalScore(b, 10);
+
+             System.out.println("Recursive test out: " + test1);
+             System.out.println("Iterative test out: " + test2);
          }
-
-
-        IterApproach iterApproach = new IterApproach();
-        iterApproach.FindOptimalScore(arr,10);
-
-        int test = FindOptimalScore(b,10);
 
         scan.close();
-        System.out.println("Test out: " + test);
      }
-     /* FIXME: clean up self checking code
-      *
-      */
+
+
      public static int FindOptimalScore(int[] a, int t) {
          if (a.length == 1) {
              //System.out.println("length is 1");
