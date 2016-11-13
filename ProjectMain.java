1b998bbb3deaf213e0ddf961705d07f5ff6662b7
Austin Mann <amann@prnhelpdesk.com>
2016-11-13 15:55:21 -0500

    Reorganized Source files

--- ProjectMain.java
+++ ProjectMain.java
@@ -14,43 +14,17 @@ import java.util.Arrays;
         int[] e = {5,2,3,4,10,5,3};
         int[] f = {8,2,3,4,5,9,1,10,3,4,2,8};
     
-        int test1;
-        test1 = IterApproach.FindOptimalScore(e,10);
+        int test1 = IterApproach.FindOptimalScore(e,10);
      
-        int test2 = FindOptimalScore(b,10);
+        int test2 = RecurseApproach.FindOptimalScore(e,10);
 
+        int test3 = RecurseMem.FindOptimalScore(e,10);
+
+        System.out.println("Recursive, Memoized test out: " + test3);
         System.out.println("Recursive test out: " + test2);
         System.out.println("Iterative test out: " + test1);
 
 
      }
-     /* FIXME: clean up self checking code
-      *
-      */
-     public static int FindOptimalScore(int[] a, int t) {
-         if (a.length == 1) {
-             //System.out.println("length is 1");
-             int ineq = (t - a[0])*(t - a[0]);
-             //System.out.println("returned " + ineq);
-             return ineq;
-         }
-         int min = (t - a[0])*(t - a[0]) + FindOptimalScore(Arrays.copyOfRange(a,1,a.length),t);
-         //Known that a.length is at least 2, so attempt to consolidate
-         for (int i = 2; i <= a.length; i++) {
-             if (t >= Helpers.getSubsetSum(a,i)) {
-                 int ineq = Helpers.getScore(a,t,i);
-                 //System.out.println("ineq is " + ineq);
-                 if (i <= a.length-1) {
-                     //System.out.println("recurse...");
-                     ineq += FindOptimalScore(Arrays.copyOfRange(a,i,a.length),t);
-                 }
-                 if (ineq < min) {
-                     //System.out.println("ineq (" + ineq + ") is less than min (" + min + ")");
-                     min = ineq;
-                 }
-             }
-         }
-         //System.out.println("returning " + min);
-         return min;
-     }
+     
  }
